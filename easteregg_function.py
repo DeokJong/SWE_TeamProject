88042c3056ae6a0123f75686f48bed8cd17664cb
haezeong <128895800+haezeong@users.noreply.github.com>
2023-11-09 23:26:02 +0900

    Create easteregg_function.py

---
+++ easteregg_function.py
@@ -0,0 +1,19 @@
+#202116317 kim hye-jeong
+
+def easteregg_function(variable):
+    if variable == '777':
+        print("Lucky Number!")
+
+
+# def main():
+    # Implement get_variable_function
+    # variable = ?
+    
+    # Call easteregg_function with the variable you want to check
+    # easteregg_function(variable)
+    
+    # Implement validation function
+
+    # Implement operate function
+
+# main()
