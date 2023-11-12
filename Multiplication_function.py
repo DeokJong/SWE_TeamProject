9b6664506ae00dbed612ccf476f4bed466b409f7
Nicknaek <135965042+Nicknaek@users.noreply.github.com>
2023-11-12 18:24:12 +0900

    Update Multiplication_function.py

--- Multiplication_function.py
+++ Multiplication_function.py
@@ -1,25 +1,16 @@
 #201910083_니키 
-# Implementation for multiplication_function
-#
-# First, check for '*'.
-# Second, split the expression to create a list of numbers.
-# Third, multiply all the numbers in the list.
-# Fourth, return the result.
 
- def multiplication_function(expression: str) -> float:
-    try:
-        # Check for '*'
-        if '*' in expression:
-            
-            num1, num2 = map(float, expression.split('*'))
-            
-            # Multiply the numbers
-            result = num1 * num2
-            print("Result:", result)
-        
-        else:
-            print("Error: Invalid input.")
-    
-    except (ValueError, Exception) as e:
-        print(f"Error: {e}")
-multiplication_function(input("Enter a mathematical expression: "))
+def calculate_product():
+    # multiplication problem
+ 
+    user_input = input("Input a multiplication problem: ")
+
+    if ' times ' in user_input:
+        # Extract the numbers 
+        operand1, operand2 = map(int, user_input.split(' times '))
+
+        # calculate 
+        result = operand1 * operand2
+        print("The result of multiplying {} and {} is: {}".format(operand1, operand2, result))
+    else:
+        print("Wrong input")
