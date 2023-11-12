cd076ec309fb2690a0b3d972ceb675b2114a1d2c
jeongwoo1998 <56586533+jeongwoo1998@users.noreply.github.com>
2023-11-12 22:26:41 +0900

    Caluculator.py

--- Caluculator.py
+++ Caluculator.py
@@ -1,11 +1,15 @@
+# 201918351 장정우
+# 코드 파트 별 통합 최종본
+
 def calculator():
-    expression = []
+    expression = ""
     
     while True:
         user_input = input("User input: ")
-            
+        expression += user_input
+
         if user_input == '=':
-            if is_valid_expession(expression):
+            if is_valid_expression(expression):
 
                 operator = expression[1]
 
@@ -20,56 +24,122 @@ def calculator():
                     print("=", result)
                 break
             else:
-                print("= ERROR!")
+                print_error_message()
                 break
 
         elif easteregg_function(user_input):
             print("lucky number!")
             break
 
-        else:
-            expression.append(user_input)
+def is_integer(variable : str) -> bool:
+    """
+    매개변수로 들어온 문자열이 정수인지 확인합니다.
 
-def is_valid_expession(expression): # 입력 값 유효성 검사
+    Parameters:
+    - variable (str) : 문자열
+
+    Returns:
+    - bool : 문자열이 유효한 정수이면 True, 그렇지 않으면 False.
+    """
     
-    for i in range(0, len(expression), 2): # 정수 값 입력 확인
-        if not expression[i].isdigit():
-            return False
-        
-    current_operator = ""
-    for i in range(1, len(expression), 2): # 연산자 확인 (사용가능한 연산자 + 하나의 연산자만 사용)
+    try:
+        int(variable)
+        return True
+    
+    except ValueError:
+        return False
+    
+def is_valid_operator(operator : str) -> bool:
+    """
+    매개변수로 들어온 문자열이 유효한 연산자인지 확인합니다.
 
-        if expression[i] not in {'+', '-', '*'}:
-            return False
+    Parameters:
+    - operator (str) : 문자열
+
+    Returns:
+    - bool : 문자열이 유효한 연산자 (+, -, *)이면 True, 그렇지 않으면 False.
+    """
+   
+    return operator in ('+', '-', '*')
+
+
+def is_valid_expression(expression : str) -> bool:
+    """
+    매개변수로 들어온 계산식이 유효한 계산식인지 확인합니다.
+
+    Parameters:
+    - expression (str) : 계산식
+
+    Returns:
+    - bool : 계산식이 유효하면 True, 그렇지 않으면 False.
+    """
+    
+    currentOperator:str = ""
+    count = 0
+
+    for i in range(len(expression) - 1):
+        if count % 2 == 0:
+            if not is_integer(expression[i]):
+                return False
+            
+        else:
+            if not is_valid_operator(expression[i]):
+                return False
         
-        if current_operator == "":
-            current_operator = expression[i]
-        elif current_operator != expression[i]:
-            return False
+            if currentOperator == "":
+                currentOperator = expression[i]
+
+            elif currentOperator != expression[i]:
+                return False
+
+        count += 1
+
+    if expression[-1] != "=":
+        return False
         
     return True
+
+def print_error_message() -> None:
+    """
+    유효하지 않은 계산식인 경우 오류 메시지를 출력합니다.
+    """
+    print("ERROR!")
 
 def easteregg_function(user_input): # 이스터에그 처리
     if user_input == "777":
         return True
 
-def addition_function(expression): # 덧셈 연산 수행
-    result = 0
-    for i in range(0, len(expression), 2):
-        result += int(expression[i])
-    return result
+def addition_function(expression) -> int: # 덧셈 연산 수행
+    expression = expression.strip('=') #remove '='
+    number_list = expression.split('+') #divide expression to make list
 
-def subtract_function(expression): # 뺄샘 연산 수행
-    result = 0
-    for i in range(0, len(expression), 2):
-        result -= int(expression[i])
-    return result
+    reslut = int(0) # initiate reslut variable
+    for number in number_list: # addition
+        reslut += int(number)
 
-def multiplication_function(expression): # 곱셈 연산 수행
-    result = 1
-    for i in range(0, len(expression), 2):
-        result *= int(expression[i])
-    return result
+    return reslut 
+
+def subtract_function(expression) -> int: # 뺄샘 연산 수행
+    expression = expression.strip('=') #remove '='
+    number_list = expression.split('-') #divide expression to make list
+
+    reslut = int(number_list[0]) # initiate reslut variable
+    
+    for number in number_list[1:]: # subtract
+        reslut -= int(number)
+
+    return reslut 
+
+def multiplication_function(expression) -> int: # 곱셈 연산 수행
+    expression = expression.strip('=') #remove '='
+    number_list = expression.split('*') #divide expression to make list
+
+    reslut = int(1) # initiate reslut variable
+    
+    for number in number_list: # multiplication
+        reslut *= int(number)
+
+    return reslut 
 
 if __name__ == "__main__":
     calculator()
